1f15450e7fa94b9e89ddd6ed72351da1d18f2be9
Anna Randeberg <anna-randeberg@hotmail.com>
2025-05-01 20:10:32 +0200

    Set up Firebase and exported auth instance

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAHxJUlh6XgQtr16eq--fZzHvuKqZxCB5g",
+    authDomain: "spk-hytta.firebaseapp.com",
+    projectId: "spk-hytta",
+    storageBucket: "spk-hytta.appspot.com",
+    messagingSenderId: "508319635743",
+    appId: "1:508319635743:web:7c72263bea1d3720df382a",
+    measurementId: "G-XZ49LK0DYK"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
